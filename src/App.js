f1ee2c6a488d9618b670123ffead724549687987
miha11-hash <d_mihaela@msn.com>
2024-03-15 08:50:51 +0200

    install use-local-storage & App JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useLocalStorage from "use-local-storage";
+import { FaToggleOn } from "react-icons/fa";
+
 
 function App() {
   return (
-    <div>
-      hello
+    <div className="app">
+      <div className="login">
+        <h1>Login</h1>
+        <form>
+          <label>Email</label>
+          <input type="email" placeholder="Enter your email"></input>
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password"></input>
+          <div className="remember">
+            <input type="checkbox" checked></input>
+            <p>Remember me</p>
+          </div>
+          <button>Log In</button>
+        </form>
+        <div className="bottom">
+          <p>Forget your password?</p>
+          <a href="/">Reset password</a>
+          <p>Create Account</p>
+        </div>
+      </div>
+      <div className="theme-toggle">
+        <h2>Light Theme</h2>
+        <FaToggleOn></FaToggleOn>
+      </div>
     </div>
   );
 }
